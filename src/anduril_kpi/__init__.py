44ffe3639275716cf67dd524f90a6997f979482b
Giles Royster <royster.giles@gmail.com>
2022-07-16 17:48:03 -0400

    Created a load endpoint to test each section. Future: dbify to turn file into database ready files

--- src/anduril_kpi/__init__.py
+++ src/anduril_kpi/__init__.py
@@ -1,4 +1,31 @@
+# Standard Imports
 from pathlib import Path
 
-ROOT_FOLDER = Path(__file__).parent
-DATA_FOLDER = ROOT_FOLDER.joinpath('data')
+ROOT_PATH = Path(__file__).parent
+DATA_PATH = ROOT_PATH.joinpath('data')
+DATABASE_PATH = ROOT_PATH.joinpath('database')
+
+def dbify():
+    """
+        Purpose: Turn the files into a database
+    """
+    pass
+
+def load():
+    # from anduril_kpi.models.company import companies
+    # print("Companies: ", companies())
+
+    # from anduril_kpi.models.industry import industries
+    # print("Industries: ", industries())
+
+    # from anduril_kpi.models.kpi import kpis
+    # print("KPIs: ", kpis())
+
+    # from anduril_kpi.models.kpi_meta import kpis_meta
+    # print("KPIs Meta: ", kpis_meta())
+
+    # from anduril_kpi.models.kpi_tags import kpis_tags
+    # print("KPIs DF Tags: ", kpis_tags())
+
+    from anduril_kpi.models.kpi_tags import kpis_tags
+    print("KPIs List Tags: ", kpis_tags(return_type='list'))
---
+++ src/anduril_kpi/main.py
@@ -0,0 +1,4 @@
+from anduril_kpi import load
+
+if __name__ == "__main__":
+    app = load()
